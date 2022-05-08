76178360b20133424c8d5ea01430dabfcb269391
dachanh <ghostcodert7@gmail.com>
2022-05-09 02:18:17 +0700

    Update product of array except self 2

--- leetcode/Product_of_Array_Except_Self/main.go
+++ leetcode/Product_of_Array_Except_Self/main.go
@@ -1,19 +1,18 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	leftProduct := make([]int,len(nums))
-	rightProduct := make([]int,len(nums))
+	leftProduct := 1
+	rightProduct := 1
 	res := make([]int,len(nums))
-	rightProduct[0] = 1
-	leftProduct[len(nums)-1] = 1
+	res[0] = 1
+	res[len(nums)-1] = 1
 	for r:= 1 ; r <= len(nums) -1 ; r++{
-		rightProduct[r] = nums[r-1]*rightProduct[r-1]
+		rightProduct = nums[r-1]*rightProduct
+		res[r] = rightProduct
 	}
 	for l:= len(nums)-2; l >= 0; l--{
-		leftProduct[l] = nums[l+1]*leftProduct[l+1]
-	}
-	for it:= 0; it < len(nums);it++{
-		res[it] =rightProduct[it]*leftProduct[it]
+		leftProduct = leftProduct*nums[l+1]
+		res[l] = leftProduct*res[l]
 	}
 	return res
 }
