5071f2b536d072b913b9705482c8ce8eaa42478e
dachanh <ghostcodert7@gmail.com>
2022-06-22 13:56:55 +0700

    UPDATE

---
+++ fpt_test/test6/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func ADD(T string) string {
+	return fmt.Sprintf("ADD %s", T[len(T)-1])
+}
+
+func CheckSameString(S, T string) bool {
+	Sslice := []string(strings.Split(S, ""))
+	Tslice := []string(strings.Split(T, ""))
+	sort.Strings(Sslice)
+	sort.Strings(Tslice)
+	newS := strings.Join(Sslice, "")
+	newT := strings.Join(Tslice, "")
+	return newS == newT
+}
+
+func Change(S, T string) string {
+	countChangeLetter := 0
+	var result string
+	for i := 0; i < len(S); i++ {
+		if S[i] != T[i] {
+			countChangeLetter += 1
+			result = fmt.Sprintf("CHANGE %s %s", S[i], T[i])
+		}
+	}
+	if countChangeLetter > 1 {
+		return "NOT_RESULT"
+	}
+	return result
+}
+
+func Solution(S string, T string) string {
+	if len(T) == len(S) {
+		if S == T {
+			return "NOTHING"
+		}
+		if !CheckSameString(S, T) {
+			for i := 0; i < len(S); i++ {
+				if S[i] != T[i] {
+					return fmt.Sprintf("MOVE %s", S[i])
+				}
+			}
+		} else {
+			result := Change(S, T)
+			if result == "NOT_RESULT" {
+				return "IMPOSSIBLE"
+			}
+			return result
+		}
+	} else if len(T)-len(S) == 1 {
+		return ADD(T)
+	}
+	return "IMPOSSIBLE"
+}
